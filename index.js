8fb74e3fcf05745ecd38c3cd6df3c0d60bff6c3c
野狩 <ningyi.zny@alibaba-inc.com>
2015-01-12 02:12:58 +0800

    version

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 require.config({
-  // baseUrl: "",
   paths: {
     'zepto': '/bower_components/zepto/zepto',
     'util': '/bower_components/wechat-util/index',
   },
-  // shim: {
-  //   'zepto': {　　　　　　　　
-  //     exports: '$1'　　　　　　
-  //   },
-  // }
 });
-define(['zepto','./src/controller', './src/loading'], function(w, Controller, Loading) {
+define(function(require) {
+  require('zepto');
+  var Controller = require('./src/controller.js');
+  var Loading = require('./src/loading.js');
+
   var loading = new Loading();
   loading.begin(function(imgs) {
     new Controller($('.container'), imgs);
